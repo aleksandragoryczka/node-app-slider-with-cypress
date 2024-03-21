1a139e702d1f5108f8d1d67a45f3da342ba8bfe1
Aleksandra <91892932+aleksandragoryczka@users.noreply.github.com>
2024-03-21 20:26:25 +0100

    Add HotJar tracking

--- cypress/e2e/spec.cy.js
+++ cypress/e2e/spec.cy.js
@@ -16,3 +16,17 @@ describe('Swiper Gallery Test', function () {
     cy.get('.swiper-slide-active').should('contain', 'Paris');
   });
 });
+
+describe('Swiper Gallery Test', function () {
+  it('Checks if gallery is displayed correctly', function() {
+    cy.visit('http://localhost:3000');
+    cy.get('.swiper').should('be.visible');
+    cy.get('.swiper-slide').should('be.visible');
+    cy.get('.swiper-slide-active').should('be.visible');
+    cy.get('.swiper-button-next').should('be.visible');
+    cy.get('.swiper-button-prev').should('be.visible');
+    cy.get('.swiper-button-next').should('be.clickable');
+    cy.get('.swiper-button-prev').should('be.clickable');
+  })
+
+})
